c19c582826d8874c69598b2ea04140edadc3133a
Rana Agastya <asr93@zoo.cs.yale.edu>
2022-12-18 14:06:09 -0500

    Added structure for learning agents

--- dqn.py
+++ dqn.py
@@ -1,8 +1,38 @@
 import numpy as np
 import random
 import itertools as it
+import torch
+import torch.nn as nn
+
+## Need a learning and a target network network
+
+## Need to define parameters of each network (which should be the same)
+## Number of each kinda card in own deck
+## How do we capture the agent knowing that the opponent is out of a single color - need only the last #num_players cards on the deck
+
+def conv_state_to_tensor(state):
+    """ Returns a PyTorch tensor"""
+    pass
 
 
-class DQN():
+class DQNAgent():
+    def __init__(self):
+        ## Create Learning, Target Network
+        pass
     
+    def train(self, num_rounds=100000, batch_update=1000):
+        ## Train by running sims on the target network, storing in database and then updating learning
+
+
+class DQN(nn.Module):
+    def __init__(self, input_size, output_size):
+        ## Duplicate for target network
+        super().__init__()
+
+        # Define constants.
+        self.latent_space_size = latent_space_size
+
+    def forward(self, state):
+        pass
+
     
--- qlearning.py
+++ qlearning.py
@@ -4,6 +4,20 @@ import random
 import numpy as np
 from bisect import bisect
 
+## We want to approximate the state space as:
+## - Num cards of each color that we have
+## - Num of each special card that we have 
+## - Num of each special card in the discard pile color (playable)
+## - Num of cards all the other players have
+## - Discard pile rank (could discretize as number for num) and color
+## - Could also add discard pile number of cards of each color played already
+
+## Action space as:
+## - Draw card
+## - Playing R, S, D, W, WD
+## - Playing a same color card
+## - Playing a same rank card
+
 class QLearningAgent():
     def __init__(self, num_states, num_actions, alpha=0.1, gamma=0.9, epsilon=0.1):
         self.num_states = num_states
@@ -16,6 +30,7 @@ class QLearningAgent():
         self.last_action = None
 
     def get_action(self, state):
+        ## Need to change this to allow for the differing number of actions
         if random.random() < self.epsilon:
             return random.randint(0, self.num_actions - 1)
         else:
