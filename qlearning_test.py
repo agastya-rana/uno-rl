dc8fea5fcc3293696022ec2754886dd7e1387199
Elven <53184599+Elv100@users.noreply.github.com>
2022-12-18 20:27:23 -0500

    test

--- qlearning_test.py
+++ qlearning_test.py
@@ -1,7 +1,11 @@
+from turtle import st
 from qlearning import *
 
 if __name__ == "__main__":
     agent = QLearningAgent(2*93312,8)
-    agent.train_and_save("qlearning.npy", timelimit=2000)
-    #agent.train_and_save("qlearning.npy", timelimit=None, gamelimit=10000)
+    # agent.train_and_save("qlearning.npy", timelimit=2000)
+    start = time.time()
+    gamesNum = 1000000
+    agent.train_and_save("qlearning.npy", timelimit=None, gamelimit=gamesNum)
     print(agent.load_and_simulate("qlearning.npy", 1000))
+    print(f'GamesNum: {gamesNum}, Duration: {time.time()-start}')
