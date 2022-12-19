fe6e7d06e1e6d6d4a3294420b84bc1ef7d2c48f4
Elv100 <elvenshum@gmail.com>
2022-12-18 19:08:15 -0500

    games_limit impl

--- qlearning_test.py
+++ qlearning_test.py
@@ -2,5 +2,6 @@ from qlearning import *
 
 if __name__ == "__main__":
     agent = QLearningAgent(2*93312,8)
-    agent.train_and_save("qlearning.npy", 2000)
+    agent.train_and_save("qlearning.npy", timelimit=2000)
+    #agent.train_and_save("qlearning.npy", timelimit=None, gamelimit=100000)
     print(agent.load_and_simulate("qlearning.npy", 1000))
